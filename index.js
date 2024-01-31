1f6ee4741d30696ad5434161bd4df04682df41b3
Yogapriya <yogapriya2310@gmail.com>
2024-01-31 13:53:57 +0530

    basic structure created

---
+++ index.js
@@ -0,0 +1,24 @@
+const lists = [
+    {checked: "",
+    list: "wakeup early",
+
+    },
+    {checked: "",
+    list: "do exercise",
+     
+    },
+    {checked: "",
+    list: "code everyday",
+     
+    },
+    {checked: "",
+    list: "eat healthy",
+     
+    }
+
+]
+
+const li = document.createElement("li")
+const ul = document.getElementsByTagName("ul")
+
+ 
